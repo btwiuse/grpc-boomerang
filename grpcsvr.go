e425c8971a445e9d19ec5eb3ac4450d7d173a73b
navigaid <navigaid@gmail.com>
2019-11-20 15:27:29 +0800

    add tls support to grpcsvr.go

--- grpcsvr.go
+++ grpcsvr.go
@@ -8,8 +8,6 @@ import (
 	"net/http"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/reflection"
 	"grpc.go4.org/grpclog"
 
 	"github.com/btwiuse/grpc-boomerang/pkg/api"
@@ -18,31 +16,39 @@ import (
 )
 
 var (
-	addr  = flag.String("addr", "localhost:9090", "http service address")
-	creds credentials.TransportCredentials
+	port            = flag.String("port", ":9090", "http service address")
+	enableTls       = flag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")
+	tlsCertFilePath = flag.String("tls_cert_file", "localhost.pem", "Path to the CRT/PEM file.")
+	tlsKeyFilePath  = flag.String("tls_key_file", "localhost-key.pem", "Path to the private key file.")
 )
 
 func main() {
 	flag.Parse()
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
-	log.Println("listening on", *addr)
-
 	grpcServer := grpc.NewServer()
-	reflection.Register(grpcServer)
 	api.RegisterApiServer(grpcServer, &impl.ApiService{})
 
 	wrappedServer := grpcweb.WrapServer(grpcServer)
+	fileServer := http.FileServer(http.Dir("ts"))
 
-	http.Handle("/", http.FileServer(http.Dir("ts")))
 	http.Handle("/Api/", wrappedServer)
+	http.Handle("/", fileServer)
 
 	httpServer := http.Server{
-		Addr:    *addr,
+		Addr:    *port,
 		Handler: http.DefaultServeMux,
 	}
 
-	if err := httpServer.ListenAndServe(); err != nil {
-		grpclog.Fatalf("failed starting http server: %v", err)
+	if *enableTls {
+		grpclog.Printf("Starting server. https://localhost%s", *port)
+		if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
+			grpclog.Fatalf("failed starting http2 server: %v", err)
+		}
+	} else {
+		grpclog.Printf("Starting server. http://localhost%s", *port)
+		if err := httpServer.ListenAndServe(); err != nil {
+			grpclog.Fatalf("failed starting http server: %v", err)
+		}
 	}
 }
